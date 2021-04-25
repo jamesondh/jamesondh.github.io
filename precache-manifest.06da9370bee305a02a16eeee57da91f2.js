a89da278199736588f2533a5f9387cf45d0a4278
Jameson Hodge <contact@jamesonhodge.com>
2021-04-25 17:24:00 -0400

    Updates

---
+++ precache-manifest.06da9370bee305a02a16eeee57da91f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b573e38c7734d45bbe2d55390a26798",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9967b73889c0130ee914",
+    "url": "/static/css/main.afedcd1c.chunk.css"
+  },
+  {
+    "revision": "c912a5b99559e72c7d34",
+    "url": "/static/js/2.b4561fcf.chunk.js"
+  },
+  {
+    "revision": "9967b73889c0130ee914",
+    "url": "/static/js/main.8ef6365c.chunk.js"
+  },
+  {
+    "revision": "aa1d0f1c672be7083a94",
+    "url": "/static/js/runtime~main.afeea630.js"
+  }
+]);
